4780cba1cde1c297e5c754b7924184ffe7ba8659
Stanislav Ivashchenko <isn-88@ya.ru>
2021-10-29 18:08:38 +0700

    Test pass

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,7 +1,8 @@
 package koschei.models;
 
-public class Island2 {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Island2 {
     private Wood3 wood;
 
     public Island2(Wood3 wood) {
